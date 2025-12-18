07a1504d99f6bded24e457c47259977457f1798a
Harshith <23eg107b26@anurag.edu.in>
2025-12-19 01:01:22 +0530

    routes

--- client/server.js
+++ client/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+// SPA routing: serve index.html for all routes
+app.get(/^\/.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
